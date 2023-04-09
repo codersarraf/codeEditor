37c2645c2a06cd8037902e08bb824317f596b06d
amitkr <amitsarraf6@gmail.com>
2023-04-09 21:45:40 +0530

    initial commit

--- src/component/CodeSpace/CodeSpace.jsx
+++ src/component/CodeSpace/CodeSpace.jsx
@@ -2,18 +2,24 @@ import React, { useContext } from 'react'
 import CodeEditor from '../CodeEditor/CodeEditor'
 import { Box,styled } from '@mui/material'
 import { Data } from '../DataContext/DataContext'
+import '../CodeSpace/codespace.css'
 
 
 const Container = styled(Box)`
   display:flex;
   background-color : #060606;
   height:58vh;
-`
+  @media(max-width:600px){
+        display:flex;
+        flex-direction: column;
+        height: auto;
+  }
+`;
 
 function CodeSpace() {
   const {html,setHtml, css, setCss, js, setJs} = useContext(Data);
   return (
-    <Container id>
+    <Container>
         <CodeEditor headingName="HTML" icon="/" color="#FF3C41" value={html} onChange={setHtml} />
         <CodeEditor headingName="CSS" icon="*" color="#0EBEFF" value={css} onChange={setCss}/>
         <CodeEditor headingName="JavaScript" icon="{}" color="#FCD000" value={js} onChange={setJs}/>
